7b089612765873ca3666d4f09d809f0b52e17791
Lisiur <lisiur@163.com>
2015-12-17 20:27:24 +0800

    fix the bug about the out link

--- app/models/admin/site.rb
+++ app/models/admin/site.rb
@@ -1,4 +1,14 @@
 class Admin::Site < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  before_create :check_url
+  before_update :check_url
+
+  def check_url
+    if self.url =~ /(http:\/\/|ftp:\/\/|https:\/\/)/
+
+    else
+      self.url = "http://#{self.url}"
+    end
+  end
 end
